d5121dd30c5ff038642ca1ee3abc76c397c92ee8
lijing <jingli14@tsinghua.org.cn>
2022-06-27 07:32:36 +0800

    first commit

---
+++ target/debug/deps/rust_tour-eaad149e10cd7030.d
@@ -0,0 +1,5 @@
+/Users/lijing/Desktop/coder-life/00-myCode/Rust/rust_tour/target/debug/deps/rust_tour-eaad149e10cd7030.rmeta: src/main.rs
+
+/Users/lijing/Desktop/coder-life/00-myCode/Rust/rust_tour/target/debug/deps/rust_tour-eaad149e10cd7030.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/rust_tour-fb020dac2ff70149.d
@@ -0,0 +1,5 @@
+/Users/lijing/Desktop/coder-life/00-myCode/Rust/rust_tour/target/debug/deps/rust_tour-fb020dac2ff70149.rmeta: src/main.rs
+
+/Users/lijing/Desktop/coder-life/00-myCode/Rust/rust_tour/target/debug/deps/rust_tour-fb020dac2ff70149.d: src/main.rs
+
+src/main.rs:
